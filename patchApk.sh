34e4245ef02da3f56fc0bafae76fad0a394ad410
Juan Patricio <juanmuscaria@gmail.com>
2022-06-14 13:02:14 -0300

    Patch scrip update, added suggestions from #3

--- patchApk.sh
+++ patchApk.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# Objection is needed to patch the apk for nonrooted devices.
-objection patchapk -s base.apk -c config.json -l _agent.js --architecture armeabi-v7a
+
+# Manifest patching, maybe try to introduce it while objection rebuild the apk using apktool instead of doubling the work?
+apktool d base.apk -o ./.patch/
+# Patch suggested by @JarlPenguin
+# Enables audio to be recorded as well game save data to be kept after the app is uninstalled
+sed -i 's|<application android:debuggable="false"|<application android:debuggable="false" android:allowAudioPlaybackCapture="true" android:hasFragileUserData="true"|g' ./.patch//AndroidManifest.xml
+apktool b ./.patch/ -o manifest_patch.apk
+
+# Patch the apk
+objection patchapk -s manifest_patch.apk -c config.json -l _agent.js --architecture armeabi-v7a
+
+# Cleanup
+rm -rf ./.patch/
+rm -f ./manifest_patch.apk
+cp ./manifest_patch.objection.apk ./patched.apk
+rm -f ./manifest_patch.objection.apk
